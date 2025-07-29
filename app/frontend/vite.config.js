9e2a1c51b52e198ea26614e61f9785a67a59be0e
Akifali-1 <akifalilenovo@gmail.com>
2025-07-29 21:52:19 +0530

    add ons

--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   css: {
-    postcss: './postcss.config.js',
-    server: {
-      port:8080,
-      historyApiFallback: true,},  // This points to your PostCSS config
+    postcss: './postcss.config.js' // ✅ This is correct
+  },
+  server: {
+    port: 8080,
+    historyApiFallback: true       // ✅ This must be outside of css
   }
 })
